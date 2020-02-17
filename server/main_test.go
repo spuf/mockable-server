0b7f54b4ba9538192fd0698c4f913fbf178e2175
spuf <556061+spuf@users.noreply.github.com>
2020-02-17 22:10:31 +0800

    functional tests

---
+++ server/main_test.go
@@ -0,0 +1,25 @@
+package server
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestFunctional(t *testing.T) {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Done")
+	})
+
+	httpServer := httptest.NewUnstartedServer(handler)
+	defer httpServer.Close()
+
+	var logBuffer bytes.Buffer
+	srv := NewServer(httpServer.Config, log.New(&logBuffer, "", 0))
+
+	srv.Shutdown()
+	srv.ListenAndServe(func() {})
+}
