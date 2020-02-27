0e98f4640967e695bb22ab5f28868dd441282dc4
spuf <556061+spuf@users.noreply.github.com>
2020-02-27 23:52:19 +0800

    refactoring

--- middleware/graceful_shutdown_test.go
+++ middleware/graceful_shutdown_test.go
@@ -1,15 +1,32 @@
 package middleware
 
 import (
+	"bytes"
 	"context"
+	"log"
 	"net/http"
+	"strings"
 	"testing"
 )
 
 func TestListenAndServeWithGracefulShutdown(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	cancel()
-	ListenAndServeWithGracefulShutdown(ctx, &http.Server{})
 
-	// @todo: add assertions
+	var buffer bytes.Buffer
+	srv := &http.Server{
+		ErrorLog: log.New(&buffer, "", 0),
+	}
+
+	ListenAndServeWithGracefulShutdown(ctx, srv)
+
+	assertLogsContains := func(t *testing.T, logs, substr string) {
+		t.Helper()
+		if !strings.Contains(logs, substr) {
+			t.Errorf("Log does not contain:\n got: %#v\nwant substring: %#v", logs, substr)
+		}
+	}
+
+	assertLogsContains(t, buffer.String(), "Starting server to listen on")
+	assertLogsContains(t, buffer.String(), "Shutting down server")
 }
